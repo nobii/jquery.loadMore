e3a20baf2e29e4287fba009bc07f254c084c9fed
Fujisawa Shin <fujisawa-shin@kayac.com>
2013-10-29 22:02:53 +0900

    一部をクラス化するなど、コード整理。

--- jquery.loadMore.js
+++ jquery.loadMore.js
@@ -1,5 +1,5 @@
 (function ($) {
-    var accessToJson = function (json, accessor) {
+    function accessToJson (json, accessor) {
         var properties = accessor.split(/\./g);
         var res = json;
 
@@ -11,52 +11,65 @@
         });
 
         return res;
-    };
+    }
 
+    function ItemTemplate (opts) {
+        this.$base = opts.$base;
+        this.selectors = opts.selectors || {};
+        this.copyEvents = (opts.copyEvents === false) ? false : true;
+    }
+
+    ItemTemplate.prototype.renderItem = function (item) {
+        var selectors = this.selectors;
+        var $el = this.$base.clone(this.copyEvents);
+
+        $.each(selectors, function (selector, value) {
+            var $field = $el.find(selector);
+            var methods = (typeof value == 'string') ? {
+                innerHTML: value
+            } : value;
+
+            $.each(methods, function (attr, accessor) {
+                var value = (typeof accessor == 'function')
+                        ? accessor(item)
+                        : accessToJson(item, accessor);
+                
+                if (attr == 'innerHTML') {
+                    $field.html(value);
+                } else {
+                    $field.attr(attr, value);
+                }
+            });
+        });
+
+        return $el.get(0);
+    };
 
     $.fn.loadMore = function (opts) {
         // init opts
         var path = opts.path;
         var jsonAccessor = opts.jsonAccessor || '.';
-        var copyEvents = (opts.copyEvents === false) ? false : true;
-        var selectors = opts.selectors || {};
 
         if (!path) {
             throw Error('Path to resource is not found.');
         }
 
         // init elements
-        var $el = this;
-        var $root = $($el.parent().get(0));
-        var $template = $($el.get(0));
+        var $root = $(this.parent().get(0));
+        var $base = $(this.get(0));
+
+        // init template
+        var itemTemplate = new ItemTemplate({
+            $base: $base,
+            selectors: opts.selectors,
+            copyEvents: opts.copyEvents
+        });
 
         // request
         $.getJSON(opts.path, function (res) {
             var items = accessToJson(res, jsonAccessor);
-
             $.each(items, function (index, item) {
-                var $el = $template.clone(copyEvents);
-
-                $.each(selectors, function (selector, value) {
-                    var $field = $el.find(selector);
-                    var methods = (typeof value == 'string') ? {
-                        innerHTML: value
-                    } : value;
-
-                    $.each(methods, function (attr, accessor) {
-                        var value = (typeof accessor == 'function')
-                                ? accessor(item)
-                                : accessToJson(item, accessor);
-                        
-                        if (attr == 'innerHTML') {
-                            $field.html(value);
-                        } else {
-                            $field.attr(attr, value);
-                        }
-                    });
-                });
-
-                $root.append($el);
+                $root.append(itemTemplate.renderItem(item));
             });
         });
     };
